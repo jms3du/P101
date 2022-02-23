a3b82a8274f5db2fd298bb2fbd73c12ed10bd116
jms3du <josemanuel.sevillano.edu@gmail.com>
2022-02-23 13:47:17 +0100

    Inclusión de login y usuario

--- src/com/edu/model/User.java
+++ src/com/edu/model/User.java
@@ -2,4 +2,24 @@ package com.edu.model;
 
 public class User {
 
+	
+	private String login;
+	private String password;
+	
+	
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 }
