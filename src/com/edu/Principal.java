3192d655555fcc61cc6847fb4675dbc1c66c3ba7
jms3du <josemanuel.sevillano.edu@gmail.com>
2022-02-23 13:38:23 +0100

    Modificación del saludo en la clase principal

--- src/com/edu/Principal.java
+++ src/com/edu/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello, how are you doing today?");
+		System.out.println("Hello, how are you doing today, bro?");
 	}
 
 }
